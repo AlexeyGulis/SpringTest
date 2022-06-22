3ae91d82e1ac7bd5448f599e6f6b595746df2f8f
AlexeyGulis <kiri.dh@yandex.ru>
2022-06-22 16:13:12 +0300

    init,destroy,factory,scope

--- src/main/java/org/example/ClassicalMusic.java
+++ src/main/java/org/example/ClassicalMusic.java
@@ -1,6 +1,17 @@
 package org.example;
 
 public class ClassicalMusic implements Music{
+    private ClassicalMusic(){}
+
+    public static ClassicalMusic getClassicalMusic(){
+        return new ClassicalMusic();
+    }
+    public void doMyInit(){
+        System.out.println("Init method");
+    }
+    public void doMyDestroy(){
+        System.out.println("Destroy method");
+    }
     public String getSong(){
         return "Bah";
     }
